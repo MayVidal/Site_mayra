4af8ee769450e5d1225fe1afdfa23bf76c6c5060
Mayra Alencar Vidal <105242491+MayVidal@users.noreply.github.com>
2025-10-26 21:08:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // ❗ SUBSTITUA ESTE URL PELO SEU GOOGLE APPS SCRIPT PUBLICADO (Web App URL)!
-const WEB_APP_URL = https://script.google.com/macros/s/AKfycbwL7dDHYLLwA9NxWzq6PlXKHQXNEYjQrnAoID5LqO9qWRcKu_1usW6TS_A1gDRH3u6F/exec; 
+const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwL7dDHYLLwA9NxWzq6PlXKHQXNEYjQrnAoID5LqO9qWRcKu_1usW6TS_A1gDRH3u6F/exec"; 
 // ❗ Exemplo: const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbx7A_j5D.../exec";
 
 
